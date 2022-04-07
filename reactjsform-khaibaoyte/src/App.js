bf5e2ac5e2ca95ac059eb18f8ea80604f1fd86f3
Tuan2kpipo <tuan2kpipo@gmail.com>
2022-04-07 23:25:35 +0700

    first

---
+++ reactjsform-khaibaoyte/src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+import ThongTinKhaiBao from './components/ThongTinKhaiBao';
+
+
+function App() {
+  return (
+    <div className="App">
+         
+          <ThongTinKhaiBao></ThongTinKhaiBao>
+      
+   
+    </div>
+  );
+}
+
+export default App;
